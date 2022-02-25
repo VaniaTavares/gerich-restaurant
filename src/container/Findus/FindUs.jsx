e12aec4bc973a43be2a30a18ca5e495cb74971ac
Vania Tavares <vntavares56@gmail.com>
2022-02-25 11:52:57 +0000

    contacts part 1-find us

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -6,6 +6,32 @@ const FindUs = () => (
   <div className="app__bg app__wrapper section__padding" id="contact">
     <div className="app__wrapper_info">
       <SubHeading title="Contact" />
+      <h1
+        className="headtext__cormorant"
+        style={{ color: "#DCCA87", marginBottom: "2.5rem" }}
+      >
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">
+          Avenida República 56, 1900-053, Lisbon, PT (random address)
+        </p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#DCCA87", margin: "2rem 0 1rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__opensans">Mon-Fri: 10:00 am - 02:00 am </p>
+        <p className="p__opensans">Sat-Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button
+        type="button"
+        className="custom__button"
+        style={{ marginTop: "1.5rem" }}
+      >
+        Visit Us
+      </button>
     </div>
     <div className="app__wrapper_img">
       <img src={images.findus} alt="Find Us" />
